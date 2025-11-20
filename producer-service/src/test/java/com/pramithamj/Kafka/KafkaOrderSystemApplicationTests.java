b23a5016abee69df5b3ba3b369a24fa4584235ab
Pramitha Jayasooriya <lpramithamj@gmail.com>
2025-11-20 16:13:58 +0530

    feat: Initialize consumer and producer services for Kafka order system
    - Added consumer-service with Spring Boot application setup including pom.xml, application.properties, and main application class.
    - Implemented basic test structure for consumer-service.
    - Created Docker infrastructure for Kafka with a docker-compose.yml file and environment variables.
    - Added producer-service with similar Spring Boot application setup including pom.xml, application.properties, and main application class.
    - Implemented basic test structure for producer-service.
    - Configured Maven wrapper for both services.

---
+++ producer-service/src/test/java/com/pramithamj/Kafka/KafkaOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pramithamj.Kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
